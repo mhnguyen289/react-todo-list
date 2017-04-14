aadfeda4f86de6d5d4dfddc7dacf7ee44a856eeb
mnguyen <mnguyen@mnguyens-MacBook-Pro.local>
2017-04-14 12:24:30 -0700

    finished to do list

---
+++ components/main.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {AddNewTask} from './addtask';
+import {TodoAppList} from './applist';
+
+export class Todo extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {tasks: props.tasks};
+        this.updateList = this.updateList.bind(this);
+        this.removeTask = this.removeTask.bind(this);
+    }
+
+    updateList(text) {
+        var updatedTasks = this.state.tasks;
+        updatedTasks.unshift(text);
+        this.setState({tasks: updatedTasks});
+        this.updateLocalStorage(updatedTasks);
+    }
+
+    removeTask(text){
+        var updatedTasks = this.state.tasks;
+        updatedTasks.splice(updatedTasks.indexOf(text), 1);
+        this.setState({tasks: updatedTasks});
+        this.updateLocalStorage(updatedTasks);
+    }
+
+    updateLocalStorage(updatedTasks) {
+        console.log("tasks updated")
+        localStorage.setItem('storedTasks', JSON.stringify(updatedTasks))
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Todo App</h1>
+                <AddNewTask updateList={this.updateList} />
+                <TodoAppList tasks={this.state.tasks} remove={this.removeTask} />
+            </div>
+        );
+    }
+}
---
+++ script.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Todo} from './components/main'
+
+var tasksList = ["task 1", "task2", "task3"];
+
+var tasks = localStorage.getItem('storedTasks');
+if(tasks) {
+    tasksList = JSON.parse(tasks) 
+    //can only store  a string value 
+}
+
+ReactDOM.render(
+    <Todo tasks = {tasksList} />,
+    document.getElementById('todo')
+);
+
+
